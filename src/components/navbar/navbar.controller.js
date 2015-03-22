bad85fff74b078fda4c225f39410cae7ae960a2e
José Moreno <jose.moreno@reyesic.net>
2015-03-22 10:24:30 +0000

    Fix sidebar

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -3,31 +3,11 @@
 angular.module('jciapp')
   .controller('NavbarCtrl', function($scope, $timeout, $mdSidenav, $log) {
     $scope.toggleLeft = function() {
-      $mdSidenav('left').toggle()
-        .then(function(){
-          $log.debug("toggle left is done");
-        });
-    };
-    $scope.toggleRight = function() {
-      $mdSidenav('right').toggle()
-        .then(function(){
-          $log.debug("toggle RIGHT is done");
-        });
+      $mdSidenav('left').toggle();
     };
   })
   .controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
     $scope.close = function() {
-      $mdSidenav('left').close()
-      .then(function(){
-        $log.debug("close LEFT is done");
-      });
-    };
-  })
-  .controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function() {
-      $mdSidenav('right').close()
-        .then(function(){
-          $log.debug("close RIGHT is done");
-        });
+      $mdSidenav('left').close();
     };
   });
